4e856068890d8a03295ef85ae29ff8c63a6d2a26
fucheng <1615575039@qq.com>
2021-08-03 17:16:02 +0800

    修改app

--- warehouse/src/App.vue
+++ warehouse/src/App.vue
@@ -22,7 +22,7 @@
         this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
       }
 
-      //在页面刷新时将vuex里的信息保存到sessionStorage里
+      //在页面刷新时将vuex里的信息保存到sessionStorage
       window.addEventListener("beforeunload",()=>{
         sessionStorage.setItem("store",JSON.stringify(this.$store.state))
       })
